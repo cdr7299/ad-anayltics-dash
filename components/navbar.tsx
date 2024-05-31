c6aa6f29b081b34f313c143d80c683b64d8b1353
Vineet Sawhney <gadgetgeek95@gmail.com>
2024-06-01 00:17:01 +0530

    chore: remove unused sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,14 +3,21 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <>
-      <nav className="flex justify-between items-center px-4 py-4 border-b-[1px] border-[#aaa] text-primary-foreground backdrop-blur-3xl">
-        <div className="flex-grow">
-          <ul className="flex gap-4 items-center justify-center">
-            <Link href="/">Home</Link>
-            <Link href="/analytics">Analytics</Link>
-          </ul>
-        </div>
-        <div className="flex items-center"></div>
+      <nav className="flex w-full justify-center items-center px-4 py-4 border-b-[1px] border-[#aaa] text-primary-foreground backdrop-blur-3xl">
+        <ul className="flex gap-4 items-center justify-center text-base font-semibold bg-slate-500/20 rounded-lg py-1 px-8">
+          <Link
+            href="/"
+            className="px-4 py-1 rounded-md hover:bg-red-400/60 transition h-full"
+          >
+            Home
+          </Link>
+          <Link
+            href="/analytics"
+            className="px-4 py-1 rounded-md hover:bg-red-400/60 transition"
+          >
+            Analytics
+          </Link>
+        </ul>
       </nav>
     </>
   );
